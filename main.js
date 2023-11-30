738d505720cf7418828f34bec6339a04422699f7
Jackson Gravino <jacksonjlgravino@gmail.com>
2023-11-30 12:04:07 -0300

    yt

--- main.js
+++ main.js
@@ -1,73 +1,70 @@
-const elementos = document.querySelectorAll('.opcao-jogador div > img')
-let jogadorOpt = ''
-let computadorOpt = ''
+const elementos = document.querySelectorAll(".opcao-jogador div > img");
+let jogadorOpt = "";
+let computadorOpt = "";
 
 function resetOpacityJogador() {
   for (let i = 0; i < elementos.length; i++) {
-    elementos[i].style.opacity = 0.3
+    elementos[i].style.opacity = 0.3;
   }
 }
 
-function resetComputador(opcaoComputador) {
+function resetCoputador(opcaoComputador) {
   for (let i = 0; i < opcaoComputador.length; i++) {
-    opcaoComputador[i].style.opacity = 0.3
+    opcaoComputador[i].style.opacity = 0.3;
   }
 }
 
 function validarVitoria() {
-  let vencedor = document.querySelector('.resultado')
-  if (jogadorOpt == 'pedra') {
-    if (computadorOpt == 'pedra') {
-      vencedor.innerHTML = 'O jogo foi empatado'
-      console.log(vencedor)
-    } else if (computadorOpt == 'papel') {
-      vencedor.innerHTML = 'O computador ganhou'
-      console.log(vencedor)
-    } else if (computadorOpt == 'tesoura') {
-      vencedor.innerHTML = 'Você Ganhou!!!'
-      console.log(vencedor)
+  let vencedor = document.querySelector("#resultado");
+  if (jogadorOpt == "pedra") {
+    if (computadorOpt == "pedra") {
+      vencedor.innerHTML = "O jogo foi empatado";
+    } else if (computadorOpt == "papel") {
+      vencedor.innerHTML = "O computador ganhou";
+    } else if (computadorOpt == "tesoura") {
+      vencedor.innerHTML = "Você Ganhou!!!";
     }
-  } else if (jogadorOpt == 'papel') {
-    if (computadorOpt == 'pedra') {
-      vencedor.innerHTML = 'Você Ganhou!!!'
-    } else if (computadorOpt == 'papel') {
-      vencedor.innerHTML = 'O jogo foi empatado'
-    } else if (computadorOpt == 'tesoura') {
-      vencedor.innerHTML = 'O computador ganhou'
+  } else if (jogadorOpt == "papel") {
+    if (computadorOpt == "pedra") {
+      vencedor.innerHTML = "Você Ganhou!!!";
+    } else if (computadorOpt == "papel") {
+      vencedor.innerHTML = "O jogo foi empatado";
+    } else if (computadorOpt == "tesoura") {
+      vencedor.innerHTML = "O computador ganhou";
     }
-  } else if (jogadorOpt == 'tesoura') {
-    if (computadorOpt == 'pedra') {
-      vencedor.innerHTML = 'O computador ganhou'
-    } else if (computadorOpt == 'papel') {
-      vencedor.innerHTML = 'Você Ganhou!!!'
-    } else if (computadorOpt == 'tesoura') {
-      vencedor.innerHTML = 'O jogo foi empatado'
+  } else if (jogadorOpt == "tesoura") {
+    if (computadorOpt == "pedra") {
+      vencedor.innerHTML = "O computador ganhou";
+    } else if (computadorOpt == "papel") {
+      vencedor.innerHTML = "Você Ganhou!!!";
+    } else if (computadorOpt == "tesoura") {
+      vencedor.innerHTML = "O jogo foi empatado";
     }
   }
 }
 
 function computadorJogar() {
-  let rand = Math.floor(Math.random() * 3)
+  let rand = Math.floor(Math.random() * 3);
   const opcaoComputador = document.querySelectorAll(
-    '.opcao-computador div > img'
-  )
-  resetComputador(opcaoComputador)
+    ".opcao-computador div > img"
+  );
+  resetCoputador(opcaoComputador);
 
   for (let i = 0; i < opcaoComputador.length; i++) {
     if (i == rand) {
-      opcaoComputador[i].style.opacity = 1
-      computadorOpt = opcaoComputador[i].getAttribute('opt')
+      opcaoComputador[i].style.opacity = 1;
+      computadorOpt = opcaoComputador[i].getAttribute("opt");
     }
   }
-  validarVitoria()
+  validarVitoria();
 }
 
 for (let i = 0; i < elementos.length; i++) {
-  elementos[i].addEventListener('click', function (t) {
-    resetOpacityJogador()
+  elementos[i].addEventListener("click", function (t) {
+    resetOpacityJogador();
 
-    t.target.style.opacity = 1
-    jogadorOpt = t.target.getAttribute('opt')
-    computadorJogar()
-  })
+    t.target.style.opacity = 1;
+    jogadorOpt = t.target.getAttribute("opt");
+    computadorJogar();
+  });
 }
